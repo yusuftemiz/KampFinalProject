9fd7981a46722801ec14126162ecd40ee2f4c088
yusuf temiz <yusuftemiz22@gmail.com>
2021-02-13 16:11:43 +0300

    13.2

---
+++ Core/Utilities/Results/Result.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    public class Result : IResult
+    {
+        public Result(bool success, string message):this(success) //bu classın(result) tek success alan constructor ını da çalıştır.
+        {
+            Message = message;
+            //set olmasa da constructor içinde set yapabiliyoruz.
+        }
+        public Result(bool success)
+        {
+            Success = success;
+            //set olmasa da constructor içinde set yapabiliyoruz.
+        }
+
+        public bool Success { get; }
+
+        public string Message { get; }
+    }
+}
