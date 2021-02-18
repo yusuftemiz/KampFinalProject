2eba0ca04b230e7fe3a28860336b6418c6bd1621
yusuf temiz <yusuftemiz22@gmail.com>
2021-02-18 22:52:05 +0300

    13.02 ders sonu

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -36,7 +36,7 @@ namespace Business.Concrete
         {
             //İş kodları
             //Yetkisi var mı gibi ifler 
-            if (DateTime.Now.Hour==22)
+            if (DateTime.Now.Hour==23)
             {
                 return new ErrorDataResult<List<Product>>(Messages.MaintenanceTimes);
             }
